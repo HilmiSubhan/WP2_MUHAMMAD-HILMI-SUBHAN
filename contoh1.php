624a1eacfa5bee7f801b7256aef10bf55e7ac52a
Hilmi Subhan <130976661+HilmiSubhan@users.noreply.github.com>
2023-09-26 16:40:08 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controllers;
+
+class contoh1 extends BaseController
+{
+    Public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Hilmi
+        saya tinggal di bekasi
+        olah raga yang saya sukai adalah
+        bola";
+    }
+}
+?>
---
+++ latihan1.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controllers;
+
+class latihan1 extends BaseController
+{
+    Public function index()
+    {
+        echo "<P>Perkenalkan<P>";
+        echo "<ol>
+        <li>Nama :Muhammad Hilmi Subhan</li>
+        <li>Kelas : 12.3b.24</li>
+        <li>Alamat : Perum Bekasi Timur Regensi Blok O/6 No 72</li>
+        <li>Nomor Hp : 082142427918</li>
+        </ol>";
+    }
+}
+?>
